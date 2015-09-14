88e048ea1b1eeeadb68d8d0ae2423936929237ad
realbot <realbot@w3soft.ru>
2015-09-14 22:58:01 +0300

    Добавил комментарий к основнойму методу парсинга

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,6 +2,10 @@ class Parser
 
   AVALIBLE_CURRENCY = RemoteRates::PATHS.map(&:values).flatten.uniq
 
+  # Возвращает вложенный хеш вставок с допустимыми 
+  # валютами из AVALIBLE_CURRENCY
+  # 
+  # @return [Hash] Вложенный хеш ставок
   def data
     raise NoMethodError.new("No method #data for parser \"#{self.class}\"")
   end

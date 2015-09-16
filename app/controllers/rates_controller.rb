51b14d6c68cd3b9b9722830e029b12eaf343ca38
realbot <realbot@w3soft.ru>
2015-09-17 02:10:47 +0300

    Привел в порядок rates#current - вызов функционала, тесты и удалил хелпер

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,9 +1,15 @@
 class RatesController < ApplicationController
 
+  before_filter :ensure_json_request, only: :current
+
   def index
   end
 
   def current
+    render json: {
+      USD: Stats.new('USD').represent,
+      EUR: Stats.new('EUR').represent
+    }
   end
 
 end
--- spec/controllers/rates_controller_spec.rb
+++ spec/controllers/rates_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe RatesController, type: :controller do
 
+  render_views
+
+  context "GET current" do
+
+    context "Not json request" do
+      before { get :current }
+
+      it { response.status.should == 406 }
+      it { JSON.parse(response.body)['error'].should_not be_nil }
+    end
+
+    context "With json format" do
+      before { get :current, format: 'json' }
+
+      it { should respond_with(:success) }
+    end
+
+  end
+
 end

c80896d457c45eee504c5c3489971b89e0ed8ad3
realbot <realbot@w3soft.ru>
2015-09-18 18:38:00 +0300

    Изменил вызов тригеров в одном цикле

--- app/jobs/alert_trigger_job.rb
+++ app/jobs/alert_trigger_job.rb
@@ -2,9 +2,9 @@ class AlertTriggerJob < ActiveJob::Base
   queue_as :default
 
   def perform(currency, ask, bid)
-    Alert.by(currency, 'sell', 'more', ask).map(&:trigger)
-    Alert.by(currency, 'sell', 'less', ask).map(&:trigger)
-    Alert.by(currency, 'buy', 'more', bid).map(&:trigger)
-    Alert.by(currency, 'buy', 'less', bid).map(&:trigger)
+    ( Alert.by(currency, 'sell', 'more', ask) +
+      Alert.by(currency, 'sell', 'less', ask) +
+      Alert.by(currency, 'buy', 'more', bid) +
+      Alert.by(currency, 'buy', 'less', bid) ).map(&:trigger)
   end
 end
